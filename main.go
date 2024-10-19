0614dfd0a73a46532599008117c6baebc92bfcbb
Veikko <kiis.veikko@gmail.com>
2024-10-19 19:54:04 +0300

    tailwind works, projects setup done.

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"VK-GoWebsite/handlers"
 	"log"
 	"net/http"
+	"text/template"
 )
-
+// output.css and styles.css does not work
 func main() {
 	// Serve static files from the "static" directory
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
+	fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 	// Set up routes
-	http.HandleFunc("/", handlers.HomeHandler)
-	http.HandleFunc("/about", handlers.AboutHandler)
-	http.HandleFunc("/projects", handlers.ProjectsHandler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/projects", projectsHandler)
 
 	// Start the server
 	log.Println("Starting server on :8080...")
@@ -21,4 +21,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func renderTemplate(w http.ResponseWriter, templateName string) {
+    tmpl, err := template.ParseFiles("templates/layout.html", "templates/header.html", "templates/footer.html", templateName)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    tmpl.ExecuteTemplate(w, "layout", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "templates/home.html")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "templates/about.html")
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "templates/projects.html")
 }
